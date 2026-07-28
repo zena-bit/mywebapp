3192d136f131de0fa301d8635e5db3980c38d871
Catherine <catezena8@gmail.com>
2026-07-28 11:44:22 +0300

    Remove phone number and configure production settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bc)fnv-14=74vn9r5p^yqae=f)3lw=jb^mbep)k=!$okcz00hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ 'Catherine.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
