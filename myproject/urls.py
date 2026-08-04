8ac8b51186b6e401959e50e95c856a2c56e4ca75
Catherine <catezena8@gmail.com>
2026-08-04 12:16:58 +0300

    fix: add favicon route and link to resolve 404 favicon logs

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/img/product-3.png', permanent=True)),
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
